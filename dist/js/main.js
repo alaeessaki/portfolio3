92ee18fb8958507010e406f3bf6b9d7d4fd944bc
Alae Es-saki <alaeessaki@gmail.com>
2019-11-10 13:53:30 +0100

    this comment

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,13 @@
-console.log("test");
+var links = document.querySelectorAll(".navs-item");
+var active_link;
+links.forEach(element => {
+    element.addEventListener("click", () => {
+        element.classList.add("active");
+        active_link = element;
+        links.forEach(element => {
+            if (element != active_link) {
+                element.classList.remove("active");
+            }
+        })
+    });
+});
